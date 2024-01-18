affbad98b563e9e0b28ce2c13d023321736cf400
Bruno Risso <brunorisso58@gmail.com>
2024-01-18 19:07:52 -0300

    PrayerLife #001 - Creating Migrations

---
+++ database/migrations/2024_01_18_215543_create_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_answered');
+            $table->dateTime('answered_date');
+            $table->foreignId('category_id')->constrained();
+            $table->unsignedBigInteger('privacy_id');
+            $table->foreign('privacy_id')->references('id')->on('privacy');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
